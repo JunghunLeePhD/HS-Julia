303d8f475e8fcbf416602293b8e0e6818728e0b0
JunghunLeePhD <yijeonghunxd@gmail.com>
2025-11-27 16:06:22 +0900

    Feat/Poly(`Polynomial.hs`): add 'zeros', 'normalize', and 'degree' functions

--- src/Polynomial.hs
+++ src/Polynomial.hs
@@ -9,3 +9,24 @@ eval :: Poly -> (Complex Double -> Complex Double)
 eval cpoly z = sum $ zipWith (*) cpoly zPowers
     where
         zPowers = iterate (*z) 1 :: [Complx Double]
+
+isComplexZero :: Complex Double -> Bool
+isComplexZero c = magnitude c < epsilon
+    where 
+        epsilon :: Double
+        epsilon = 1.0e-12
+
+isPolyZero :: Poly -> Bool
+isPolyZero cpoly = all isComplexZero cpoly 
+
+normalize :: Poly -> Poly
+normalize poly = dropWhileEnd isComplexZero poly
+
+degree :: Poly -> Int
+degree cpoly = 
+    let 
+        normalizedPoly = normalize cpoly
+    in 
+        case normalizedPoly of
+            [] -> 0          
+            xs -> length xs - 1
